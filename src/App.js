01ecc3213527b9c8ee4910de9690472d9bd0f3e6
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-04 16:26:43 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./MusicPlayer.css";
 
-function App() {
+const MusicPlayer = () => {
+  const [playing, setPlaying] = useState(false);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const audioRef = useRef(null);
+
+  const songs = [    {      url: "https://cdn.discordapp.com/attachments/852241547327307786/1070622679666806795/starboy-scloudtomp3downloader.com.mp3",      title: "Song 1",    },   
+                     {      url: "https://cdn.discordapp.com/attachments/852241547327307786/1070622679666806795/starboy-scloudtomp3downloader.com.mp3",      title: "Song 2",    },    
+                     {      url: "https://cdn.discordapp.com/attachments/852241547327307786/1070622679666806795/starboy-scloudtomp3downloader.com.mp3",      title: "Song 3",    },  ];
+
+  const playPause = () => {
+    if (playing) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlaying(!playing);
+  };
+
+  const skip = (forward) => {
+    let nextIndex = currentSongIndex + (forward ? 1 : -1);
+    if (nextIndex >= songs.length) {
+      nextIndex = 0;
+    } else if (nextIndex < 0) {
+      nextIndex = songs.length - 1;
+    }
+    setCurrentSongIndex(nextIndex);
+    setPlaying(true);
+    audioRef.current.src = songs[nextIndex].url;
+    audioRef.current.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="music-player">
+      <audio ref={audioRef} />
+      <p>{songs[currentSongIndex].title}</p>
+      <button onClick={() => playPause()} className="play-pause">{playing ? "Pause" : "Play"}</button>
+      <button onClick={() => skip(false)}>Previous</button>
+      <button onClick={() => skip(true)}>Next</button>
     </div>
   );
-}
+  
+};
 
-export default App;
+export default MusicPlayer;
